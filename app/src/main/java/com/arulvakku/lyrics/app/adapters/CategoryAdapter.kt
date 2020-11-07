ab331e9f7969dd20c559aeeb49954742d07cc774
Vivek Ellappan <vivekebiit@gmail.com>
2020-11-07 13:56:16 +0530

    color changes done

--- app/src/main/java/com/arulvakku/lyrics/app/adapters/CategoryAdapter.kt
+++ app/src/main/java/com/arulvakku/lyrics/app/adapters/CategoryAdapter.kt
@@ -1,6 +1,7 @@
 package com.arulvakku.lyrics.app.adapters
 
 import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
@@ -10,6 +11,7 @@ import com.arulvakku.lyrics.app.R
 import com.arulvakku.lyrics.app.data.Category
 import com.arulvakku.lyrics.app.databinding.LayoutCategoryRowItemBinding
 import com.sembiyan.songs.app.listeners.CellClickListener
+import kotlinx.android.synthetic.main.layout_category_row_item.view.*
 
 class CategoryAdapter(
     var context: Context,
@@ -33,6 +35,11 @@ class CategoryAdapter(
 
     override fun onBindViewHolder(holder: CategoryAdapter.ViewHolder, position: Int) {
         holder.bind(items.get(position))
+
+        holder.itemView.materialCardView.setCardBackgroundColor(Color.parseColor(items.get(position).color_code))
+        holder.itemView.materialCardView.radius = 10F
+        holder.itemView.materialCardView.elevation = 5F
+
         holder.itemView.setOnClickListener {
             cellClickListener.onCellClickListener(items.get(position).title)
         }
@@ -41,6 +48,7 @@ class CategoryAdapter(
     // Creating ViewHolder
     class ViewHolder(val binding: LayoutCategoryRowItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
+
         fun bind(data: Any) {
             binding.setVariable(
                 BR.category,
