439e60b1a1e6c8694756274e29d9cc0de7cfde5a
Naveen <naveen@mercury.cash>
2020-11-07 13:50:24 +0530

    about screen added and menu added

---
+++ app/src/main/java/com/arulvakku/lyrics/app/activities/AboutActivity.kt
@@ -0,0 +1,38 @@
+package com.arulvakku.lyrics.app.activities
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.arulvakku.lyrics.app.R
+import kotlinx.android.synthetic.main.activity_about.*
+
+class AboutActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+        supportActionBar?.hide()
+
+        initActions()
+    }
+
+    private fun initActions() {
+        back.setOnClickListener { finish() }
+
+        nineth_one.setOnClickListener { openMail() }
+
+        fourth_one.setOnClickListener { openBrowser()  }
+    }
+
+    private fun openMail() {
+        val email =
+            Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", getString(R.string.email), null))
+        startActivity(Intent.createChooser(email, getString(R.string.app_name)))
+    }
+
+    private fun openBrowser() {
+        val uri = Uri.parse(getString(R.string.base_url))
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+        startActivity(intent)
+    }
+}
---
+++ app/src/main/java/com/arulvakku/lyrics/app/activities/BaseActivity.kt
@@ -0,0 +1,45 @@
+package com.arulvakku.lyrics.app.activities
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.arulvakku.lyrics.app.R
+
+
+open class BaseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_base)
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id: Int = item.itemId
+        return when (id) {
+            R.id.about -> {
+                startActivity(Intent(applicationContext, AboutActivity::class.java))
+                true
+            }
+            R.id.feedback -> {
+                openMail()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    fun openMail() {
+        val email =
+            Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", getString(R.string.email), null))
+        startActivity(Intent.createChooser(email, getString(R.string.app_name)))
+    }
+
+}
