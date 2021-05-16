5cecd81d5ad316cc7f43c1c41e0a1f739517217e
test <test>
2021-05-16 12:11:50 +0530

    basic code setup

---
+++ app/src/main/java/com/arulvakku/lyrics/app/activities/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.arulvakku.lyrics.app.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.arulvakku.lyrics.app.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
--- app/src/main/java/com/arulvakku/lyrics/app/activities/SplashActivity.kt
+++ app/src/main/java/com/arulvakku/lyrics/app/activities/SplashActivity.kt
@@ -1,5 +1,6 @@
 package com.arulvakku.lyrics.app.activities
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
@@ -7,7 +8,16 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        startMainActivity()
     }
+
+    private fun startMainActivity() {
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
 }
 
 
