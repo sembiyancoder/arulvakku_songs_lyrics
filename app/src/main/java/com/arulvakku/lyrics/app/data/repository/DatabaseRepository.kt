69fbf9a9386bc6a943e88a5351cb029f70cdc412
Livin <livingstonantony70@gmail.com>
2021-06-11 08:24:58 +0530

    re-commit
    Methods added in the following fragments
    LyricsFragment.kt
    1. isFavouriteSong
    2. setFavouriteSong
    
    FavouriteFragment.kt
    1. getFavouriteSongs
    2. removeFavouriteSong

--- app/src/main/java/com/arulvakku/lyrics/app/data/repository/DatabaseRepository.kt
+++ app/src/main/java/com/arulvakku/lyrics/app/data/repository/DatabaseRepository.kt
@@ -1,6 +1,5 @@
 package com.arulvakku.lyrics.app.data.repository
 
-import com.arulvakku.lyrics.app.ui.view.home.category.SongCategoryModel
 import com.arulvakku.lyrics.app.ui.view.home.category.cache.SongCategoryCacheMapper
 import com.arulvakku.lyrics.app.ui.view.home.category.cache.SongCategoryDao
 import com.arulvakku.lyrics.app.ui.view.home.song.SongModel
@@ -8,6 +7,7 @@ import com.arulvakku.lyrics.app.ui.view.home.song.cache.SongCacheMapper
 import com.arulvakku.lyrics.app.ui.view.home.song.cache.SongDao
 import com.arulvakku.lyrics.app.ui.view.favourite.cache.PlaylistSongCrossRef
 import com.arulvakku.lyrics.app.ui.view.favourite.cache.PlaylistWithSongs
+import com.arulvakku.lyrics.app.ui.view.home.model.SongCategoryModel
 import javax.inject.Inject
 
 class DatabaseRepository
